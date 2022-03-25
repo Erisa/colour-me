6159b198fa87bfca35b614e385b6f0dd3db6acdc
Erisa A <erisa@erisa.uk>
2022-03-25 00:17:59 +0000

    Clean up

--- src/commands/colour-role.ts
+++ src/commands/colour-role.ts
@@ -42,67 +42,68 @@ export default class BotCommand extends SlashCommand {
   }
 
   async run(ctx: CommandContext) {
-    if (ctx.member?.permissions.has(1 << 28)) {
-      console.info(ctx.options[ctx.subcommands[0]].role )
-
-      const roleId = ctx.options[ctx.subcommands[0]].role;
-
-      // @ts-ignore
-      const kvResult = await kv.get(ctx.guildID);
-
-      let roleList: string[] = [];
-
-      if (kvResult !== null){
-        roleList = JSON.parse(kvResult);
-      }
-
-      let embedcontent: string = "";
-
-      switch (ctx.subcommands[0]){
-        case 'add':
-          if (roleList.includes(roleId)){
-            ctx.send({
-              content: `The role <@&${roleId}> is already in the colour role list!`,
-              ephemeral: true
-            });
-          } else {
-            roleList.push(roleId);
-            // @ts-ignore
-            await kv.put(ctx.guildID, JSON.stringify(roleList));
-            return `I've added <@&${roleId}> to the colour role list!`;
-          }
-          break;
-        case 'remove':
-          if (roleList.includes(roleId)){
-            roleList.splice(roleList.indexOf(roleId), 1);
-            // @ts-ignore
-            await kv.put(ctx.guildID, JSON.stringify(roleList));
-            return `I've removed <@&${roleId}> from the colour role list!`;
-          } else {
-            ctx.send({
-              content: `The role <@&${roleId}> wasn't on the colour role list!`,
-              ephemeral: true
-            });
-          }
-          break;
-        case 'list':
-          roleList.forEach((listRole: string) => (embedcontent += ` <@&${listRole}>`));
-
-          ctx.send({
-            content: 'These are all the configured roles. It might include deleted roles though.',
-            embeds: [
-              {
-                description: embedcontent
-              }
-            ]
-          });
-          break;
-      }
-    } else {
+    if (!ctx.member?.permissions.has(1 << 28)) {
       ctx.send({
         content: 'You need `Manage Roles` permission to use this command.',
         ephemeral: true
       });
+      return;
+    }
+
+    console.info(ctx.options[ctx.subcommands[0]].role);
+
+    const roleId = ctx.options[ctx.subcommands[0]].role;
+
+    // @ts-ignore
+    const kvResult = await kv.get(ctx.guildID);
+
+    let roleList: string[] = [];
+
+    if (kvResult !== null) {
+      roleList = JSON.parse(kvResult);
+    }
+
+    let embedcontent: string = '';
+
+    switch (ctx.subcommands[0]) {
+      case 'add':
+        if (roleList.includes(roleId)) {
+          ctx.send({
+            content: `The role <@&${roleId}> is already in the colour role list!`,
+            ephemeral: true
+          });
+        } else {
+          roleList.push(roleId);
+          // @ts-ignore
+          await kv.put(ctx.guildID, JSON.stringify(roleList));
+          return `I've added <@&${roleId}> to the colour role list!`;
+        }
+        break;
+      case 'remove':
+        if (roleList.includes(roleId)) {
+          roleList.splice(roleList.indexOf(roleId), 1);
+          // @ts-ignore
+          await kv.put(ctx.guildID, JSON.stringify(roleList));
+          return `I've removed <@&${roleId}> from the colour role list!`;
+        } else {
+          ctx.send({
+            content: `The role <@&${roleId}> wasn't on the colour role list!`,
+            ephemeral: true
+          });
+        }
+        break;
+      case 'list':
+        roleList.forEach((listRole: string) => (embedcontent += ` <@&${listRole}>`));
+
+        ctx.send({
+          content: 'These are all the configured roles. It might include deleted roles though.',
+          embeds: [
+            {
+              description: embedcontent
+            }
+          ]
+        });
+        break;
     }
   }
 }
